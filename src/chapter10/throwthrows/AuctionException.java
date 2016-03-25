da45d724dea99dd595c5982901bd44fbbbdf7b19
chenweiqiang <chenweiqiang2016@163.com>
2016-03-25 10:47:23 +0800

    自定义异常的构造方法(Throwable t)

--- src/chapter10/throwthrows/AuctionException.java
+++ src/chapter10/throwthrows/AuctionException.java
@@ -1,5 +1,9 @@
 package chapter10.throwthrows;
 
+/**
+ * 增加最后一个构造方法 可以传入一个异常对象 当抛出异常时 实现异常的跟踪
+ */
+
 public class AuctionException extends Exception{
 
 	private static final long serialVersionUID = 1L;
@@ -9,5 +13,9 @@ public class AuctionException extends Exception{
 	
 	public AuctionException(String msg){
 		super(msg);
+	}
+	
+	public AuctionException(Throwable t){
+		super(t);
 	}
 }
