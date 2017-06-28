65e5b16c6170f04a48b02362cc554e258bd75869
陈伟强 <chenweiqiang2016@163.com>
2017-06-28 08:48:23 +0800

    Signed-off-by: 陈伟强 <chenweiqiang2016@163.com>

---
+++ src/chapter15/FilenameFilterTest.java
@@ -0,0 +1,13 @@
+import java.io.File;
+import java.io.FilenameFilter;
+
+
+public class FilenameFilterTest{
+    public static void main(String[] args){
+//目标类型是FilenameFilter 实现里面的accept方法
+String[] names = new File(".").list((dir,name) -> name.endsWith(".java") || new  File(name).isDirectory());
+for(String name:names){
+System.out.println(name);
+}
+}
+}
